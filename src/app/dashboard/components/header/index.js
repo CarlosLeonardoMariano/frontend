8b9bf930958184ece9980250c47adb9628ebd198
CARLOS LEONARDO MARIANO <carloslmariano2000@gmail.com>
2025-04-24 10:57:21 -0300

    mensagem do commit

--- src/app/dashboard/components/header/index.js
+++ src/app/dashboard/components/header/index.js
@@ -1,62 +1,48 @@
-    "use client"
+"use client";
 
-    import Link from 'next/link';
-    import styles from './styles.module.scss'; 
-    import { LogOutIcon } from 'lucide-react'; 
-    import { deleteCookie } from 'cookies-next';
-    import { useRouter } from 'next/navigation';
-    import { toast } from 'sonner';
+import Link from 'next/link';
+import styles from './styles.module.scss'; 
+import { LogOutIcon } from 'lucide-react'; 
+import { deleteCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
-    export default function Header() {
-        const router = useRouter();
-
-        // função para fazer logout
+export default function Header() {
+    const router = useRouter();
 
     async function handleLogout() {
-        deleteCookie("tokens", {path: "/"})
+        deleteCookie("tokens", { path: "/" });
         toast.success('Sua sessão foi encerrada com sucesso. Agradecemos pelo seu tempo. Caso precise de ajuda, não hesite em nos contactar.', {
             style: {
-                backgroundColor: '#f44336', 
-                color: 'white', 
+                backgroundColor: '#f44336',
+                color: 'white',
             }
         });
-        
-        router.replace('/')
+
+        router.replace('/');
     }
 
-
-
-
-
-
-
-
-            return (
-                <header className={styles.headerContainer}>
+    return (
+        <header className={styles.headerContainer}>
             <div className={styles.headerContante}>
-
                 <Link href={'/dashboard'}>
-                <div className={styles.logoTotal}> 
-                                Atende<p className={styles.p}>Pro</p>
-                        </div>
+                    <div className={styles.logoTotal}>
+                        Atende<p className={styles.p}>Pro</p>
+                    </div>
                 </Link>
 
                 <nav className={styles.nav}>
-    <div className={styles.menuLinks}>
-        <Link href={'/dashboard/categorias'}>Categorias</Link>
-        <Link href={'/dashboard/produtos'}>Produtos</Link>
-    </div>
-
-    <form action={handleLogout}>
-        <button type='submit'>
-        <LogOutIcon size={22} color='white' />
-        </button>
-    </form>
-</nav>
-
-
-
+                    <div className={styles.menuLinks}>
+                        <Link href={'/dashboard/categorias'}>Categorias</Link>
+                        <Link href={'/dashboard/produtos'}>Produtos</Link>
+                        <form action={handleLogout}>
+                            <button type='submit'>
+                                <LogOutIcon size={22} color='white' />
+                            </button>
+                        </form>
+                    </div>
+                </nav>
             </div>
-                </header>
-            );
-            }
+        </header>
+    );
+}
