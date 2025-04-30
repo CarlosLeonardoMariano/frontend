8cba6f2a39cfdcd91b3fc20ad5516c8ffa2fe22c
CARLOS LEONARDO MARIANO <carloslmariano2000@gmail.com>
2025-04-30 18:25:11 -0300

    comitando quarta feira

--- src/app/dashboard/financeiro/page.js
+++ src/app/dashboard/financeiro/page.js
@@ -2,9 +2,16 @@
 
 import styles from './financeiro.module.scss';  
 import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
+import Swal from 'sweetalert2';
+import { useRouter } from 'next/navigation';
+
+
 
 export default function Financeiro() {
   const [pedidos, setPedidos] = useState([]);
+
+  const router = useRouter();
 
   // Cálculos dinâmicos baseados nas movimentações
   const entradas = pedidos
@@ -41,8 +48,26 @@ export default function Financeiro() {
   function handleADDfinanceiro() {
     const valorPositivo = parseFloat(prompt('Digite um valor para adicionar ao seu caixa:'));
     if (isNaN(valorPositivo) || valorPositivo <= 0) {
-      alert('Valor inválido');
+ 
+      toast.warning('Por favor, insira um valor numérico válido antes de continuar.',{
+        style:{
+          backgroundColor:'red',
+          color:'white',
+          border:'none'
+        },
+        duration:1500
+      })
       return;
+    } else{
+      toast.success('Valor adicionado ao seu caixa com sucessos !',{
+        style:{
+          backgroundColor:'green',
+          color:'white',
+          border:'none'
+        },
+        duration:1500
+      })
+
     }
 
     const novaMovimentacao = {
@@ -82,12 +107,24 @@ export default function Financeiro() {
   function handleRemoverfinanceiro() {
     const valorNegativo = parseFloat(prompt('Digite um valor para tirar do seu caixa:'));
     if (isNaN(valorNegativo) || valorNegativo <= 0) {
-      alert('Valor inválido');
+      toast.warning('Por favor, insira um valor numérico válido antes de continuar.',{
+        style:{
+          backgroundColor:'red',
+          color:'white',
+          border:'none'
+        },
+        duration:1500
+      })
       return;
     }
 
     if (valorNegativo > saldoAtual) {
-      alert('Você não tem esse valor disponível no seu caixa!');
+      toast.error('Você não tem esse valor disponível no seu caixa!',{
+        style:{
+          background:'red',
+          fontWeight:'bold'
+        }
+      });
       return;
     }
 
@@ -103,6 +140,51 @@ export default function Financeiro() {
     const novasMovimentacoes = [...pedidos, novaMovimentacaoNegativo];
     setPedidos(novasMovimentacoes);
     localStorage.setItem('movimentacoes', JSON.stringify(novasMovimentacoes));
+  }
+
+  function handleRemoverlocalStorage(req){
+
+  const movimentacoes = localStorage.getItem('movimentacoes')
+
+  if(!movimentacoes || JSON.parse(movimentacoes).length === 0){
+    toast.info('Não há dados para remover.', {
+      style: {
+        backgroundColor: 'red',
+        color: 'white',
+        border: 'none'
+      },
+      duration: 1500
+    });
+    return;
+  }
+
+
+    localStorage.clear();
+
+    Swal.fire({
+      title: 'Deseja realmente excluir essa tabela ?',
+      showCancelButton: true,
+      confirmButtonText: 'Sim, excluir',
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#e74c3c',
+      cancelButtonColor: '#7f8c8d',
+    }).then( (result)=>{
+      if(result.isConfirmed){
+        localStorage.removeItem('movimentacoes');
+        setPedidos([]);
+
+        toast.success('Tabela de movimentações limpa com sucesso!', {
+          style: {
+            backgroundColor: '#27ae60',
+            color: '#fff',
+            border: 'none'
+          },
+          duration: 1500
+        });
+        router.refresh()
+
+      }
+    });
   }
 
   return (
@@ -128,6 +210,7 @@ export default function Financeiro() {
 
       <div className={styles.movimentacoes}>
         <h2 className={styles.h2Movimentaçoes}>Movimentações do dia</h2>
+        
         <div className={styles.tableWrapper}>
         <table >
           <thead>
@@ -163,13 +246,27 @@ export default function Financeiro() {
       </div>
 
       <div className={styles.buttons}>
+        <div className={styles.divgap}>
         <button className={`${styles.btn} ${styles.btnEntrada}`} onClick={handleADDfinanceiro}>
           Nova Entrada
         </button>
         <button className={`${styles.btn} ${styles.btnSaida}`} onClick={handleRemoverfinanceiro}>
           Nova Saída
         </button>
+
+        </div>
+      
+
+        <div>
+<button className={`${styles.btn} ${styles.btnDel}`} onClick={handleRemoverlocalStorage}>
+          Limpar Tabela
+        </button>
+</div>
+
+        
       </div>
+
+      
     </div>
   );
 }
